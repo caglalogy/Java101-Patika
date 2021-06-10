8195664a141a1645e06d0f77e76084d2e0d8582f
caglalogy <caglasahin180999@gmail.com>
2021-06-10 10:56:50 +0300

    assignment3 - updated

--- TriangleAreaCalculator/src/Main.java
+++ TriangleAreaCalculator/src/Main.java
@@ -4,7 +4,8 @@ import java.lang.Math;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        float edge1, edge2, hypo;
+        /** float edge1, edge2, hypo;
+
         System.out.println("------RIGHT TRIANGLE CALCULATOR------");
         System.out.print("enter first edge of right triangle: ");
         edge1 = input.nextFloat();
@@ -12,13 +13,27 @@ public class Main {
         edge2 = input.nextFloat();
 
         hypo = (float) Math.sqrt(Math.pow(edge1, 2) + Math.pow(edge2, 2));
-
         System.out.println("hypotenuse is " + hypo);
 
         // triangle area calculation
         float perimeter = edge1 + edge2 + hypo; //2u
         float u = perimeter/2; //u
         float area = (float) Math.sqrt(u * (u - edge1) * (u-edge2) * (u - hypo));
+        System.out.println("area is " + area);
+        */
+
+        float e1, e2, e3;
+        System.out.println("------TRIANGLE AREA CALCULATOR------");
+        System.out.print("enter first edge of the triangle: ");
+        e1 = input.nextFloat();
+        System.out.print("enter second edge of the triangle: ");
+        e2 = input.nextFloat();
+        System.out.print("enter third edge of the triangle: ");
+        e3 = input.nextFloat();
+
+        float perimeter = e1 + e2 + e3; //2u
+        float u = perimeter/2; //u
+        float area = (float) Math.sqrt(u * (u - e1) * (u-e2) * (u - e3));
         System.out.println("area is " + area);
 
     }
